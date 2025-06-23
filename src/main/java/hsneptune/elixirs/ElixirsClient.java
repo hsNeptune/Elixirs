d7c647aa4be47e48b8db0b2a8e5313f129b49a3c
LuckElixir <sriniranjan.gs@gmail.com>
2025-06-23 14:58:15 -0400

    Second commit

---
+++ src/main/java/hsneptune/elixirs/ElixirsClient.java
@@ -0,0 +1,14 @@
+
+package hsneptune.elixirs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ElixirsClient implements ClientModInitializer {
+@Override
+public void onInitializeClient(){
+
+}
+}
