d3fc7060ef6eba1f19f938fb7ab56dc21fb4c40e
LuckElixir <sriniranjan.gs@gmail.com>
2025-06-30 18:35:51 -0400

    added first structure

--- src/main/java/org/hsneptune/elixirs/worldgen/Worldgen.java
+++ src/main/java/org/hsneptune/elixirs/worldgen/Worldgen.java
@@ -7,10 +7,13 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.feature.PlacedFeature;
+import net.minecraft.world.gen.structure.Structure;
+
 import org.hsneptune.elixirs.Elixirs;
 
 public class Worldgen {
     public static final RegistryKey<PlacedFeature> POTENT_ORE_GENERATION = RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier.of("elixirs","potent_ore_generation"));
+//    public static final RegistryKey<Structure> STRUCTURE =RegistryKey.of(RegistryKeys.STRUCTURE,  Identifier.of("elixirs", "desert_tower"));
 
 
     public static void initialize() {
